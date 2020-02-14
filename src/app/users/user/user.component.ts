ab9fadf81d3a17789304ae20d9ccf5418873a34b
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-02-14 14:14:22 +0200

    Passing Query Parameters and Fragments

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -7,9 +8,10 @@ import { ActivatedRoute, Params } from '@angular/router';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
 
+  user: {id: number, name: string};
+  paramsSubscription: Subscription;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -17,13 +19,15 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-    this.route.params
+    this.paramsSubscription = this.route.params
     .subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
     });
   }
-
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
 
 }
